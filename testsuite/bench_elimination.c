922477467358b433389a32209ee6f9b1d89f48b6
Martin Albrecht <malb@informatik.uni-bremen.de>
2008-12-22 15:54:16 +0100

    allow half rank in bench_elimination

--- testsuite/bench_elimination.c
+++ testsuite/bench_elimination.c
@@ -5,6 +5,7 @@
 #include "walltime.h"
 
 int main(int argc, char **argv) {
+  int halfrank = 1;
   size_t m, n;
   unsigned long long t;
   double wt;
@@ -16,8 +17,30 @@ int main(int argc, char **argv) {
   m = atoi(argv[1]);
   n = atoi(argv[2]);
   packedmatrix *A = mzd_init(m, n);
-  mzd_randomize(A);
-  
+  if (!halfrank) {
+    mzd_randomize(A);
+  } else {
+    packedmatrix *L, *U;
+    L = mzd_init(m, m);
+    U = mzd_init(m, n);
+    mzd_randomize(U);
+    mzd_randomize(L);
+    size_t i,j;
+    for (i=0; i<m; ++i){
+      mzd_write_bit(U,i,i, 1);
+      for (j=0; j<i;++j)
+        mzd_write_bit(U,i,j, 0);
+      if (i%2)
+        for (j=i; j<n;++j)
+          mzd_write_bit(U,i,j, 0);
+      for (j=i+1; j<m;++j)
+        mzd_write_bit(L,i,j, 0);
+      mzd_write_bit(L,i,i, 1);
+    }
+    mzd_mul(A,L,U,0);
+    mzd_free(L);
+    mzd_free(U);
+  }
   wt = walltime(&clockZero);
   t = cpucycles();
   size_t r = mzd_reduce_m4ri(A, 1, 0, NULL, NULL);
