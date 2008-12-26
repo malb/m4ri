f7f43f6d14f681a9e00b557865783a2a43e84c91
Martin Albrecht <malb@informatik.uni-bremen.de>
2008-12-26 17:56:01 +0100

    yet another printf fix

--- testsuite/test_solve.c
+++ testsuite/test_solve.c
@@ -24,7 +24,7 @@ int test_pluq_solve_left(size_t m, size_t n, size_t offsetA, size_t offsetB){
 
   packedmatrix *Acopy = mzd_copy(NULL, A);
   size_t r = mzd_echelonize_m4ri(Acopy,0,0,NULL,NULL);
-  printf("solve_left m: % 4zu, n: % 4zu, r: % 4zu da: % 2zu db: % 2zu ",m, n, r, offsetA, offsetB);
+  printf("solve_left m: %4zu, n: %4zu, r: %4zu da: %2zu db: %2zu ",m, n, r, offsetA, offsetB);
   mzd_free(Acopy);
   Acopy = mzd_copy(NULL, A);
     
