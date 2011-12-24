1eb67f46ff65684847fd8a6ec4c65714f3b18ae5
Martin Albrecht <martinralbrecht@googlemail.com>
2011-12-24 21:55:55 +0100

    benchmarking & testing code for inversion

---
+++ testsuite/bench_invert.c
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+
+#include "config.h"
+#include "m4ri.h"
+#include "cpucycles.h"
+#include "benchmarking.h"
+
+struct inv_params {
+  rci_t n;
+  int direction;
+  char const *algorithm;
+};
+
+int run(void *_p, unsigned long long *data, int *data_len) {
+  struct inv_params *p = (struct inv_params *)_p;
+  *data_len = 2;
+
+  mzd_t *A = NULL, *L = NULL, *U = NULL, *B = NULL;
+
+  if (p->direction <= 0) {
+    L = mzd_init(p->n, p->n);
+    mzd_randomize(L);
+    for (rci_t i = 0; i < p->n; ++i) {
+      for (rci_t j = i + 1; j < p->n; ++j)
+        mzd_write_bit(L,i,j, 0);
+      mzd_write_bit(L,i,i, 1);
+    }
+  }
+  if (p->direction >= 0) {
+    U = mzd_init(p->n, p->n);
+    mzd_randomize(U);
+    for (rci_t i = 0; i < p->n; ++i) {
+      for (rci_t j = 0; j < i; ++j)
+        mzd_write_bit(U,i,j, 0);
+      mzd_write_bit(U,i,i, 1);
+    }
+  }
+  switch(p->direction) {
+  case 0:
+    A = mzd_mul(NULL, L, U, 0);
+    mzd_free(L);
+    mzd_free(U);
+    break;
+  case -1:
+    A = L;
+    break;
+  case 1:
+    A = U;
+    break;
+  default:
+    m4ri_die("unknown direction '%d'",p->direction);
+  };
+
+  data[0] = walltime(0);
+  data[1] = cpucycles();
+  switch(p->direction) {
+  case 0:
+    if(strcmp(p->algorithm, "m4ri") == 0)
+      B = mzd_inv_m4ri(NULL, A, 0);
+    else
+      m4ri_die("unknown algorithm: '%s'",p->algorithm);
+    break;
+  case 1:
+    if(strcmp(p->algorithm, "m4ri") == 0) {
+      mzd_inv_upper_m4ri(A, 0);
+      B = mzd_copy(NULL, A);
+    } else
+      m4ri_die("unknown algorithm: '%s'",p->algorithm);
+    break;
+  case -1:
+    m4ri_die("not implemented error");
+    break;
+  }
+  data[1] = cpucycles() - data[1];
+  data[0] = walltime(data[0]);
+  mzd_free(A);
+  mzd_free(B);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  int opts = global_options(&argc, &argv);
+
+  if (opts < 0) {
+    bench_print_global_options(stderr);
+    exit(-1);
+  }
+
+  if (argc != 4) {
+    printf("Parameters n,direction,alg expected.\n");
+    printf(" n         -- integer > 0\n");
+    printf(" direction -- lower triangular (-1), full (0) or upper triangular (1).\n");
+    printf(" algorithm -- 'm4ri'\n");
+    printf("\n");
+    bench_print_global_options(stderr);
+    m4ri_die("");
+  }
+
+  struct inv_params params;
+  params.n = atoi(argv[1]);
+  params.direction = atoi(argv[2]);
+  params.algorithm = argv[3];
+
+  /* put this call in run() to benchmark one particular matrix over
+     and over again instead of computing the average of various
+     matrices.*/
+  srandom(17);
+  unsigned long long data[2];
+  run_bench(run, (void*)&params, data, 2);
+
+  double cc_per_op = ((double)data[1])/ powl((double)params.n,2.807);
+
+  printf("n: %5d, cpu cycles: %10llu, cc/(n^2.807): %.5lf, wall time: %lf\n", params.n, data[1], cc_per_op, data[0] / 1000000.0);
+}
---
+++ testsuite/test_invert.c
@@ -0,0 +1,100 @@
+#include "config.h"
+#include <stdlib.h>
+#include "m4ri.h"
+
+/**
+ * Check that inversion works.
+ *
+ * \param m Number of rows of A
+ * \param l Number of columns of A/number of rows of B
+ * \param n Number of columns of B
+ * \param k Parameter k of M4RM algorithm, may be 0 for automatic choice.
+ * \param cutoff Cut off parameter at which dimension to switch from
+ * Strassen to M4RM
+ */
+int invert_test(rci_t n, int k) {
+  int ret  = 0;
+  printf("invert: n: %4d, k: %2d", n, k);
+
+  mzd_t *I2 = mzd_init(n,n);
+  mzd_set_ui(I2,1);
+
+  mzd_t *U = mzd_init(n,n);
+  mzd_randomize(U);
+  for(rci_t i=0; i<n; i++) {
+    mzd_write_bit(U, i, i, 1);
+    for (rci_t j=0; j<i; j++)
+      mzd_write_bit(U, i, j, 0);
+  }
+
+  mzd_t *B = mzd_copy(NULL, U);
+  mzd_inv_upper(B);
+
+  mzd_t *I1 = mzd_mul(NULL, U, B, 0);
+
+  if (mzd_equal(I1, I2) != TRUE) {
+    ret += 1;
+    printf(" U*~U != 1 ");
+  }
+
+  mzd_t *L = mzd_init(n, n);
+  mzd_randomize(L);
+  for (rci_t i = 0; i < n; ++i) {
+    for (rci_t j = i + 1; j < n; ++j)
+      mzd_write_bit(L,i,j, 0);
+    mzd_write_bit(L,i,i, 1);
+  }
+  mzd_t *A = mzd_mul(NULL, L, U, 0);
+
+  B = mzd_inv_m4ri(B,A,k);
+
+  I1 = mzd_mul(I1, A, B, 0);
+
+  if (mzd_equal(I1, I2) != TRUE) {
+    ret += 1;
+    printf(" A*~A != 1 ");
+  }
+
+  if(ret == 0) {
+    printf(" ... passed\n");
+  } else {
+    printf(" ... FAILED\n");
+  }
+  mzd_free(U);
+  mzd_free(L);
+  mzd_free(A);
+  mzd_free(B);
+  mzd_free(I1);
+  mzd_free(I2);
+
+  return ret;
+
+}
+
+int main() {
+  int status = 0;
+  srandom(17);
+
+  for(int k=0; k<7; k++) {
+    status += invert_test(   1,k);
+    status += invert_test(   2,k);
+    status += invert_test(   3,k);
+    status += invert_test(  21,k);
+    status += invert_test(  64,k);
+    status += invert_test( 128,k);
+    status += invert_test( 193,k);
+    status += invert_test(1000,k);
+    status += invert_test(1025,k);
+    status += invert_test(1290,k);
+    status += invert_test(1710,k);
+    status += invert_test(2048,k);
+    status += invert_test(4096,k);
+    status += invert_test(1024,k);
+  }
+  if (status == 0) {
+    printf("All tests passed.\n");
+    return 0;
+  } else {
+    return -1;
+  }
+}
