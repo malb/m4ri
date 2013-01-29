9bfea24c6f749fcd91ef8036165be2d8c4c21525
Martin Albrecht <martinralbrecht@googlemail.com>
2013-01-29 15:29:19 +0100

    adding mzd_process_rowsX_ple back as it is used in triangular_russian.c

--- src/ple_russian.h
+++ src/ple_russian.h
@@ -114,4 +114,72 @@ int _mzd_ple_submatrix(mzd_t *A,
  */
 mzd_t *_mzd_ple_to_e(mzd_t *E, mzd_t const *A, rci_t r, rci_t c, int k, rci_t *offsets);
 
+/**
+ * \brief add rows E0,E1 to M between startrow and stoprow, starting at startcol.
+ *
+ * \param M        Matrix
+ * \param startrow Start processing in this row
+ * \param stoprow  Stop processing in this row
+ * \param startcol Start processing in this column
+ * \param k0       Number of bits to read for E0
+ * \param T0       Lookup index -> row for E0
+ * \param E0       2^k0 x A::ncols table
+ * \param k1       Number of bits to read for E1
+ * \param T1       Lookup index -> row for E1
+ * \param E1       2^k1 x A::ncols table
+ */
+
+void mzd_process_rows2_ple(mzd_t *M, rci_t startrow, rci_t stoprow, rci_t startcol,
+                           int const k0, mzd_t const *T0, rci_t const *E0,
+                           int const k1, mzd_t const *T1, rci_t const *E1);
+
+/**
+ * \brief add rows E0,E1,E2 to M between startrow and stoprow, starting at startcol.
+ *
+ * \param M        Matrix
+ * \param startrow Start processing in this row
+ * \param stoprow  Stop processing in this row
+ * \param startcol Start processing in this column
+ * \param k0       Number of bits to read for E0
+ * \param T0       Lookup index -> row for E0
+ * \param E0       2^k0 x A::ncols table
+ * \param k1       Number of bits to read for E1
+ * \param T1       Lookup index -> row for E1
+ * \param E1       2^k1 x A::ncols table
+ * \param k2       Number of bits to read for E2
+ * \param T2       Lookup index -> row for E2
+ * \param E2       2^k2 x A::ncols table
+ */
+
+void mzd_process_rows3_ple(mzd_t *M, rci_t startrow, rci_t stoprow, rci_t startcol,
+                           int const k0, mzd_t const *T0, rci_t const *E0,
+                           int const k1, mzd_t const *T1, rci_t const *E1,
+			   int const k2, mzd_t const *T2, rci_t const *E2);
+
+/**
+ * \brief add rows E0,E1,E2,E3 to M between startrow and stoprow, starting at startcol.
+ *
+ * \param M        Matrix
+ * \param startrow Start processing in this row
+ * \param stoprow  Stop processing in this row
+ * \param startcol Start processing in this column
+ * \param k0       Number of bits to read for E0
+ * \param T0       Lookup index -> row for E0
+ * \param E0       2^k0 x A::ncols table
+ * \param k1       Number of bits to read for E1
+ * \param T1       Lookup index -> row for E1
+ * \param E1       2^k1 x A::ncols table
+ * \param k2       Number of bits to read for E2
+ * \param T2       Lookup index -> row for E2
+ * \param E2       2^k2 x A::ncols table
+ * \param k3       Number of bits to read for E3
+ * \param T3       Lookup index -> row for E3
+ * \param E3       3^k3 x A::ncols table */
+
+void mzd_process_rows4_ple(mzd_t *M, rci_t startrow, rci_t stoprow, rci_t startcol,
+                           int const k0, mzd_t const *T0, rci_t const *E0,
+                           int const k1, mzd_t const *T1, rci_t const *E1,
+                           int const k2, mzd_t const *T2, rci_t const *E2,
+                           int const k3, mzd_t const *T3, rci_t const *E3);
+
 #endif // M4RI_PLE_RUSSIAN
