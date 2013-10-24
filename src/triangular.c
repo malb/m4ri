feea07a20068ddacb60554dffdbce235478456a4
Martin Albrecht <martinralbrecht@googlemail.com>
2013-10-24 11:49:40 +0200

    cleaned up triangular.c (lower right is also faster)

--- src/triangular.c
+++ src/triangular.c
@@ -30,8 +30,6 @@
 #include "parity.h"
 
 
-#define TRSM_THRESHOLD m4ri_radix
-
 /*****************
  * UPPER RIGHT
  ****************/
@@ -63,7 +61,7 @@ void _mzd_trsm_upper_right(mzd_t const *U, mzd_t *B, const int cutoff) {
   rci_t const mb = B->nrows;
   rci_t const nb = B->ncols;
 
-  if(nb <= TRSM_THRESHOLD) {
+  if(nb <= m4ri_radix) {
     /* base case */
     _mzd_trsm_upper_right_base(U, B);
     return;
@@ -115,8 +113,6 @@ void _mzd_trsm_upper_right_base(mzd_t const *U, mzd_t *B) {
   rci_t const mb = B->nrows;
   rci_t const nb = B->ncols;
 
-  assert(m4ri_radix == 64);
-
   for(rci_t i = 1; i < nb; ++i) {
     /* Computes X_i = B_i + X_{0..i-1} U_{0..i-1,i} */
     register word ucol = 0;
@@ -130,9 +126,10 @@ void _mzd_trsm_upper_right_base(mzd_t const *U, mzd_t *B) {
     word tmp[64];
 
     for(giantstep = 0; giantstep + m4ri_radix < mb; giantstep += m4ri_radix) {
-      /* for(int babystep = 0; babystep < m4ri_radix; ++babystep) */
-      /*   tmp[babystep] = B->rows[giantstep + babystep][0] & ucol; */
-
+#if 0
+      for(int babystep = 0; babystep < m4ri_radix; ++babystep) 
+           tmp[babystep] = B->rows[giantstep + babystep][0] & ucol;
+#else
       word **src = B->rows + giantstep;
       tmp[ 0] = src[ 0][0] & ucol, tmp[ 1] = src[ 1][0] & ucol, tmp[ 2] = src[ 2][0] & ucol, tmp[ 3] = src[ 3][0] & ucol;
       tmp[ 4] = src[ 4][0] & ucol, tmp[ 5] = src[ 5][0] & ucol, tmp[ 6] = src[ 6][0] & ucol, tmp[ 7] = src[ 7][0] & ucol;
@@ -150,12 +147,13 @@ void _mzd_trsm_upper_right_base(mzd_t const *U, mzd_t *B) {
       tmp[52] = src[52][0] & ucol, tmp[53] = src[53][0] & ucol, tmp[54] = src[54][0] & ucol, tmp[55] = src[55][0] & ucol;
       tmp[56] = src[56][0] & ucol, tmp[57] = src[57][0] & ucol, tmp[58] = src[58][0] & ucol, tmp[59] = src[59][0] & ucol;
       tmp[60] = src[60][0] & ucol, tmp[61] = src[61][0] & ucol, tmp[62] = src[62][0] & ucol, tmp[63] = src[63][0] & ucol;
-
+#endif
       word const dotprod = m4ri_parity64(tmp);
-
-      /* for(int babystep = 0; babystep < m4ri_radix; ++babystep) */
-      /*   if(__M4RI_GET_BIT(dotprod, babystep)) */
-      /*     __M4RI_FLIP_BIT(B->rows[giantstep + babystep][0], i); */
+#if 0
+      for(int babystep = 0; babystep < m4ri_radix; ++babystep)
+         if(__M4RI_GET_BIT(dotprod, babystep)) 
+           __M4RI_FLIP_BIT(B->rows[giantstep + babystep][0], i);
+#else
       src[ 0][0] ^= ((dotprod>> 0)&m4ri_one)<<i, src[ 1][0] ^= ((dotprod>> 1)&m4ri_one)<<i, src[ 2][0] ^= ((dotprod>> 2)&m4ri_one)<<i, src[ 3][0] ^= ((dotprod>> 3)&m4ri_one)<<i;
       src[ 4][0] ^= ((dotprod>> 4)&m4ri_one)<<i, src[ 5][0] ^= ((dotprod>> 5)&m4ri_one)<<i, src[ 6][0] ^= ((dotprod>> 6)&m4ri_one)<<i, src[ 7][0] ^= ((dotprod>> 7)&m4ri_one)<<i;
       src[ 8][0] ^= ((dotprod>> 8)&m4ri_one)<<i, src[ 9][0] ^= ((dotprod>> 9)&m4ri_one)<<i, src[10][0] ^= ((dotprod>>10)&m4ri_one)<<i, src[11][0] ^= ((dotprod>>11)&m4ri_one)<<i;
@@ -172,9 +170,9 @@ void _mzd_trsm_upper_right_base(mzd_t const *U, mzd_t *B) {
       src[52][0] ^= ((dotprod>>52)&m4ri_one)<<i, src[53][0] ^= ((dotprod>>53)&m4ri_one)<<i, src[54][0] ^= ((dotprod>>54)&m4ri_one)<<i, src[55][0] ^= ((dotprod>>55)&m4ri_one)<<i;
       src[56][0] ^= ((dotprod>>56)&m4ri_one)<<i, src[57][0] ^= ((dotprod>>57)&m4ri_one)<<i, src[58][0] ^= ((dotprod>>58)&m4ri_one)<<i, src[59][0] ^= ((dotprod>>59)&m4ri_one)<<i;
       src[60][0] ^= ((dotprod>>60)&m4ri_one)<<i, src[61][0] ^= ((dotprod>>61)&m4ri_one)<<i, src[62][0] ^= ((dotprod>>62)&m4ri_one)<<i, src[63][0] ^= ((dotprod>>63)&m4ri_one)<<i;
+#endif
     }
-
-
+    
     for(int babystep = 0; giantstep + babystep < mb; ++babystep)
       tmp[babystep] = B->rows[giantstep + babystep][0] & ucol;
     for(int babystep = mb - giantstep; babystep < 64; ++babystep)
@@ -209,11 +207,9 @@ void _mzd_trsm_lower_right(mzd_t const *L, mzd_t *B, const int cutoff) {
   rci_t const mb = B->nrows;
   rci_t const nb = B->ncols;
 
-  if(nb <= TRSM_THRESHOLD){
-    /* base case */
-    _mzd_trsm_lower_right_base (L, B);
-    return;
-  }
+  if(nb <= m4ri_radix) 
+    return _mzd_trsm_lower_right_base (L, B);
+
   rci_t const nb1 = (((nb - 1) / m4ri_radix + 1) >> 1) * m4ri_radix;
 
   /**
@@ -259,9 +255,7 @@ void _mzd_trsm_lower_right_base(mzd_t const *L, mzd_t *B) {
   rci_t const nb = B->ncols;
 
   for(rci_t i = nb - 1; i >= 0; --i) {
-
     /* Computes X_i = B_i + X_{i+1,n} L_{i+1..n,i} */
-
     register word ucol = 0;
     for(rci_t k = i + 1; k < nb; ++k) {
       if(__M4RI_GET_BIT(L->rows[k][0], i))
@@ -272,16 +266,54 @@ void _mzd_trsm_lower_right_base(mzd_t const *L, mzd_t *B) {
     rci_t giantstep;
     word tmp[64];
     for(giantstep = 0; giantstep + m4ri_radix < mb; giantstep += m4ri_radix) {
+#if 0
       for(int babystep = 0; babystep < m4ri_radix; ++babystep)
-	tmp[babystep] = B->rows[giantstep + babystep][0] & ucol;
+           tmp[babystep] = B->rows[giantstep + babystep][0] & ucol;
+#else
+      word **src = B->rows + giantstep;
+      tmp[ 0] = src[ 0][0] & ucol, tmp[ 1] = src[ 1][0] & ucol, tmp[ 2] = src[ 2][0] & ucol, tmp[ 3] = src[ 3][0] & ucol;
+      tmp[ 4] = src[ 4][0] & ucol, tmp[ 5] = src[ 5][0] & ucol, tmp[ 6] = src[ 6][0] & ucol, tmp[ 7] = src[ 7][0] & ucol;
+      tmp[ 8] = src[ 8][0] & ucol, tmp[ 9] = src[ 9][0] & ucol, tmp[10] = src[10][0] & ucol, tmp[11] = src[11][0] & ucol;
+      tmp[12] = src[12][0] & ucol, tmp[13] = src[13][0] & ucol, tmp[14] = src[14][0] & ucol, tmp[15] = src[15][0] & ucol;
+      tmp[16] = src[16][0] & ucol, tmp[17] = src[17][0] & ucol, tmp[18] = src[18][0] & ucol, tmp[19] = src[19][0] & ucol;
+      tmp[20] = src[20][0] & ucol, tmp[21] = src[21][0] & ucol, tmp[22] = src[22][0] & ucol, tmp[23] = src[23][0] & ucol;
+      tmp[24] = src[24][0] & ucol, tmp[25] = src[25][0] & ucol, tmp[26] = src[26][0] & ucol, tmp[27] = src[27][0] & ucol;
+      tmp[28] = src[28][0] & ucol, tmp[29] = src[29][0] & ucol, tmp[30] = src[30][0] & ucol, tmp[31] = src[31][0] & ucol;
+      tmp[32] = src[32][0] & ucol, tmp[33] = src[33][0] & ucol, tmp[34] = src[34][0] & ucol, tmp[35] = src[35][0] & ucol;
+      tmp[36] = src[36][0] & ucol, tmp[37] = src[37][0] & ucol, tmp[38] = src[38][0] & ucol, tmp[39] = src[39][0] & ucol;
+      tmp[40] = src[40][0] & ucol, tmp[41] = src[41][0] & ucol, tmp[42] = src[42][0] & ucol, tmp[43] = src[43][0] & ucol;
+      tmp[44] = src[44][0] & ucol, tmp[45] = src[45][0] & ucol, tmp[46] = src[46][0] & ucol, tmp[47] = src[47][0] & ucol;
+      tmp[48] = src[48][0] & ucol, tmp[49] = src[49][0] & ucol, tmp[50] = src[50][0] & ucol, tmp[51] = src[51][0] & ucol;
+      tmp[52] = src[52][0] & ucol, tmp[53] = src[53][0] & ucol, tmp[54] = src[54][0] & ucol, tmp[55] = src[55][0] & ucol;
+      tmp[56] = src[56][0] & ucol, tmp[57] = src[57][0] & ucol, tmp[58] = src[58][0] & ucol, tmp[59] = src[59][0] & ucol;
+      tmp[60] = src[60][0] & ucol, tmp[61] = src[61][0] & ucol, tmp[62] = src[62][0] & ucol, tmp[63] = src[63][0] & ucol;
+#endif
 
       word const dotprod = m4ri_parity64(tmp);
 
+#if 0
       for(int babystep = 0; babystep < m4ri_radix; ++babystep)
-	if(__M4RI_GET_BIT(dotprod, babystep))
-	  __M4RI_FLIP_BIT(B->rows[giantstep + babystep][0], i);
+           if(__M4RI_GET_BIT(dotprod, babystep))
+             __M4RI_FLIP_BIT(B->rows[giantstep + babystep][0], i);
+#else
+      src[ 0][0] ^= ((dotprod>> 0)&m4ri_one)<<i, src[ 1][0] ^= ((dotprod>> 1)&m4ri_one)<<i, src[ 2][0] ^= ((dotprod>> 2)&m4ri_one)<<i, src[ 3][0] ^= ((dotprod>> 3)&m4ri_one)<<i;
+      src[ 4][0] ^= ((dotprod>> 4)&m4ri_one)<<i, src[ 5][0] ^= ((dotprod>> 5)&m4ri_one)<<i, src[ 6][0] ^= ((dotprod>> 6)&m4ri_one)<<i, src[ 7][0] ^= ((dotprod>> 7)&m4ri_one)<<i;
+      src[ 8][0] ^= ((dotprod>> 8)&m4ri_one)<<i, src[ 9][0] ^= ((dotprod>> 9)&m4ri_one)<<i, src[10][0] ^= ((dotprod>>10)&m4ri_one)<<i, src[11][0] ^= ((dotprod>>11)&m4ri_one)<<i;
+      src[12][0] ^= ((dotprod>>12)&m4ri_one)<<i, src[13][0] ^= ((dotprod>>13)&m4ri_one)<<i, src[14][0] ^= ((dotprod>>14)&m4ri_one)<<i, src[15][0] ^= ((dotprod>>15)&m4ri_one)<<i;
+      src[16][0] ^= ((dotprod>>16)&m4ri_one)<<i, src[17][0] ^= ((dotprod>>17)&m4ri_one)<<i, src[18][0] ^= ((dotprod>>18)&m4ri_one)<<i, src[19][0] ^= ((dotprod>>19)&m4ri_one)<<i;
+      src[20][0] ^= ((dotprod>>20)&m4ri_one)<<i, src[21][0] ^= ((dotprod>>21)&m4ri_one)<<i, src[22][0] ^= ((dotprod>>22)&m4ri_one)<<i, src[23][0] ^= ((dotprod>>23)&m4ri_one)<<i;
+      src[24][0] ^= ((dotprod>>24)&m4ri_one)<<i, src[25][0] ^= ((dotprod>>25)&m4ri_one)<<i, src[26][0] ^= ((dotprod>>26)&m4ri_one)<<i, src[27][0] ^= ((dotprod>>27)&m4ri_one)<<i;
+      src[28][0] ^= ((dotprod>>28)&m4ri_one)<<i, src[29][0] ^= ((dotprod>>29)&m4ri_one)<<i, src[30][0] ^= ((dotprod>>30)&m4ri_one)<<i, src[31][0] ^= ((dotprod>>31)&m4ri_one)<<i;
+      src[32][0] ^= ((dotprod>>32)&m4ri_one)<<i, src[33][0] ^= ((dotprod>>33)&m4ri_one)<<i, src[34][0] ^= ((dotprod>>34)&m4ri_one)<<i, src[35][0] ^= ((dotprod>>35)&m4ri_one)<<i;
+      src[36][0] ^= ((dotprod>>36)&m4ri_one)<<i, src[37][0] ^= ((dotprod>>37)&m4ri_one)<<i, src[38][0] ^= ((dotprod>>38)&m4ri_one)<<i, src[39][0] ^= ((dotprod>>39)&m4ri_one)<<i;
+      src[40][0] ^= ((dotprod>>40)&m4ri_one)<<i, src[41][0] ^= ((dotprod>>41)&m4ri_one)<<i, src[42][0] ^= ((dotprod>>42)&m4ri_one)<<i, src[43][0] ^= ((dotprod>>43)&m4ri_one)<<i;
+      src[44][0] ^= ((dotprod>>44)&m4ri_one)<<i, src[45][0] ^= ((dotprod>>45)&m4ri_one)<<i, src[46][0] ^= ((dotprod>>46)&m4ri_one)<<i, src[47][0] ^= ((dotprod>>47)&m4ri_one)<<i;
+      src[48][0] ^= ((dotprod>>48)&m4ri_one)<<i, src[49][0] ^= ((dotprod>>49)&m4ri_one)<<i, src[50][0] ^= ((dotprod>>50)&m4ri_one)<<i, src[51][0] ^= ((dotprod>>51)&m4ri_one)<<i;
+      src[52][0] ^= ((dotprod>>52)&m4ri_one)<<i, src[53][0] ^= ((dotprod>>53)&m4ri_one)<<i, src[54][0] ^= ((dotprod>>54)&m4ri_one)<<i, src[55][0] ^= ((dotprod>>55)&m4ri_one)<<i;
+      src[56][0] ^= ((dotprod>>56)&m4ri_one)<<i, src[57][0] ^= ((dotprod>>57)&m4ri_one)<<i, src[58][0] ^= ((dotprod>>58)&m4ri_one)<<i, src[59][0] ^= ((dotprod>>59)&m4ri_one)<<i;
+      src[60][0] ^= ((dotprod>>60)&m4ri_one)<<i, src[61][0] ^= ((dotprod>>61)&m4ri_one)<<i, src[62][0] ^= ((dotprod>>62)&m4ri_one)<<i, src[63][0] ^= ((dotprod>>63)&m4ri_one)<<i;
+#endif
     }
-
     for(int babystep = 0; giantstep + babystep < mb; ++babystep)
       tmp[babystep] = B->rows[giantstep + babystep][0] & ucol;
     for(int babystep = mb - giantstep; babystep < 64; ++babystep)
@@ -310,48 +342,25 @@ void mzd_trsm_lower_left(mzd_t const *L, mzd_t *B, const int cutoff) {
   _mzd_trsm_lower_left (L, B, cutoff);
 }
 
-void _mzd_trsm_lower_left(mzd_t const *L, mzd_t *B, const int cutoff) {
+ void _mzd_trsm_lower_left(mzd_t const *L, mzd_t *B, const int cutoff) {
   rci_t const mb = B->nrows;
   rci_t const nb = B->ncols;
   int const nbrest = nb % m4ri_radix;
 
-  if(mb <= m4ri_radix){
+  if(mb <= m4ri_radix) {
     /* base case */
+    word const mask_end = __M4RI_LEFT_BITMASK(nbrest);
+    for(rci_t i = 1; i < mb; ++i) {
+      /* Computes X_i = B_i + L_{i,0..i-1} X_{0..i-1}  */
+      word *Lrow = L->rows[i];
+      word *Brow = B->rows[i];
 
-    if(nb > m4ri_radix) {
-      // B is large
-      word const mask_end = __M4RI_LEFT_BITMASK(nbrest);
-
-      for(rci_t i = 1; i < mb; ++i) {
-	/* Computes X_i = B_i + L_{i,0..i-1} X_{0..i-1}  */
-        /**
-         * \todo needs to be optimized!
-         **/
-	word *Lrow = L->rows[i];
-	word *Brow = B->rows[i];
-
-	for(rci_t k = 0; k < i; ++k) {
-	  if(__M4RI_GET_BIT(Lrow[0], k)){
-	    Brow[0] ^= B->rows[k][0];
-	    for(wi_t j = 1; j < B->width - 1; ++j)
-	      Brow[j] ^= B->rows[k][j];
-	    Brow[B->width - 1] ^= B->rows[k][B->width - 1] & mask_end;
-	  }
-	}
-      }
-    } else { // B is small
-      word const mask = __M4RI_MIDDLE_BITMASK(nb, 0);
-      for(rci_t i = 1; i < mb; ++i) {
-	/* Computes X_i = B_i + L_{i,0..i-1} X_{0..i-1}  */
-	/** Need to be optimized !!! **/
-	word *Lrow = L->rows [i];
-	word *Brow = B->rows [i];
-
-	for(rci_t k = 0; k < i; ++k) {
-	  if(__M4RI_GET_BIT(Lrow[0], k)){
-	    Brow[0] ^= B->rows[k][0] & mask;
-	  }
-	}
+      for (rci_t k = 0; k < i; ++k) {
+        if (__M4RI_GET_BIT(Lrow[0], k)) {
+          for(wi_t j = 0; j < B->width - 1; ++j)
+            Brow[j] ^= B->rows[k][j];
+          Brow[B->width - 1] ^= B->rows[k][B->width - 1] & mask_end;
+        }
       }
     }
   } else {
@@ -376,7 +385,6 @@ void _mzd_trsm_lower_left(mzd_t const *L, mzd_t *B, const int cutoff) {
     mzd_free_window((mzd_t*)L10);
     mzd_free_window((mzd_t*)L11);
   }
-
   __M4RI_DD_MZD(B);
 }
 
@@ -400,38 +408,21 @@ void _mzd_trsm_upper_left(mzd_t const *U, mzd_t *B, const int cutoff) {
   if(mb <= m4ri_radix) {
     /* base case */
 
-    if(nb > m4ri_radix) {
-      // B is large
-      word const mask_end = B->high_bitmask;
+    word const mask_end = B->high_bitmask;
 
-      // U[mb-1,mb-1] = 1, so no work required for i=mb-1
-      for(rci_t i = mb - 2; i >= 0; --i) {
+    // U[mb-1,mb-1] = 1, so no work required for i=mb-1
+    for(rci_t i = mb - 2; i >= 0; --i) {
 
-        /* Computes X_i = B_i + U_{i,i+1..mb} X_{i+1..mb}  */
-        word *Urow = U->rows[i];
-        word *Brow = B->rows[i];
+      /* Computes X_i = B_i + U_{i,i+1..mb} X_{i+1..mb}  */
+      word *Urow = U->rows[i];
+      word *Brow = B->rows[i];
 
-        for(rci_t k = i + 1; k < mb; ++k) {
-          if(__M4RI_GET_BIT(Urow[0], k)){
-            for(wi_t j = 0; j < B->width - 1; ++j)
-              Brow[j] ^= B->rows[k][j];
-            Brow[B->width - 1] ^= B->rows[k][B->width - 1] & mask_end;
-          }
+      for(rci_t k = i + 1; k < mb; ++k) {
+        if(__M4RI_GET_BIT(Urow[0], k)){
+          for(wi_t j = 0; j < B->width - 1; ++j)
+            Brow[j] ^= B->rows[k][j];
+          Brow[B->width - 1] ^= B->rows[k][B->width - 1] & mask_end;
         }
-      }
-    } else { // B is small
-      word const mask_end = B->high_bitmask;
-      // U[mb-1,mb-1] = 1, so no work required for i=mb-1
-      for(rci_t i = mb - 2; i >= 0; --i) {
-	/* Computes X_i = B_i + U_{i,i+1..mb} X_{i+1..mb}  */
-	word *Urow = U->rows [i];
-	word *Brow = B->rows [i];
-
-	for(rci_t k = i + 1; k < mb; ++k) {
-	  if(__M4RI_GET_BIT(Urow[0], k)){
-	    Brow[0] ^= B->rows[k][0] & mask_end;
-	  }
-	}
       }
     }
   } else if(mb <= __M4RI_MUL_BLOCKSIZE) {
